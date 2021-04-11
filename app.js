b7c11935c42d7c6fadfef682445d8d8f3d567d17
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-04-11 11:49:36 +0530

    Added Cors Package

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.get("/", (req, res) => res.send("Hello from Render!"));
 app.get("/posts", (req, res) => {
   res.status(200).json({
